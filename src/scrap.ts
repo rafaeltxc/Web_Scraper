7fa8e53e36c6b80763af996e4eac4e0a505392d9
Rafael Costa <rafael.txct@gmail.com>
2023-03-18 15:47:10 -0300

    working scrap

--- src/scrap.ts
+++ src/scrap.ts
@@ -1,12 +1,12 @@
-import { Builder, By, Key } from 'selenium-webdriver';
-
+import { Builder, By, Key, until } from 'selenium-webdriver';
 require('chromedriver');
 interface objPattern {
-  imgSrc: string[] | null;
+  imgSrc: string | null;
   title: string | null;
   src: string | null;
   shop: string | null;
-  price: number | null;
+  price: string | null;
+  withoutDiscount: string | null;
 }
 
 (async function helloSelenium() {
@@ -20,12 +20,73 @@ interface objPattern {
 
   const shopping = await driver.findElement(By.xpath('//*[contains(text(), "Shopping")]'));
   await shopping.click();
+  const originalWindow = await driver.getWindowHandle();
+  const originalUrl = await driver.getCurrentUrl();
 
-  const sponsoredElements = await driver.findElements(By.className('KZmu8e'));
-  const data: string[] = [];
-  for (let i = 0; i < sponsoredElements.length; i++) {
-    await sponsoredElements[i].click();
+  // TODO get all elements images
+  // const productsInfos = await driver.findElements(By.className('_-da'));
+  // // const infoImgs = await driver.findElements(By.className('_-db sh-div__thumbnail'));
+  // const listImgs = [];
+  // for (let i = 0; i < productsInfos.length; i++) {
+  //   const infoImgs: WebElement[] = await productsInfos[i].findElements(By.className('_-dc'));
+
+  //   const data: string[] = [];
+  //   for (let j = 0; j < infoImgs.length; j++) {
+  //     data.push(await infoImgs[j].getAttribute('src'));
+  //   }
+  //   listImgs.push(data);
+  // }
+  // console.log(listImgs);
+
+  const elements = await driver.findElements(By.className('sh-dgr__content'));
+  const data = [];
+  for (let i = 0; i < elements.length; i++) {
+    // OPEN ELEMENT
+    // const url = await elements[i].click();
+
+    await elements[i].findElement(By.className('shntl')).click();
+    await driver.wait(async () => (await driver.getAllWindowHandles()).length === 2);
+    const windows = await driver.getAllWindowHandles();
+    windows.forEach(async (handle) => {
+      if (handle !== originalWindow) {
+        await driver.switchTo().window(handle);
+      }
+    });
+    await driver.wait(async () => (await driver.getCurrentUrl()) != originalUrl);
+    const src = await driver.getCurrentUrl();
+    await driver.close();
+    await driver.switchTo().window(originalWindow);
+
+    const imgSrc = await elements[i]
+      .findElement(By.className('ArOc1c'))
+      .findElement(By.css('img'))
+      .getAttribute('src');
+
+    const title = await elements[i].findElement(By.className('tAxDx')).getText();
+
+    const shop = await elements[i].findElement(By.className('aULzUe IuHnof')).getText();
+
+    let price;
+    let withoutDiscount = null;
+    try {
+      price = await elements[i].findElement(By.className('a8Pemb OFFNJ')).getText();
+      withoutDiscount = await elements[i].findElement(By.className('zY3Xhe OFFNJ')).getText();
+    } catch (e) {
+      price = await elements[i].findElement(By.className('a8Pemb OFFNJ')).getText();
+    }
+
+    const obj: objPattern = {
+      imgSrc,
+      title,
+      src,
+      shop,
+      price,
+      withoutDiscount,
+    };
+
+    data.push(obj);
   }
 
+  console.log(data);
   driver.close();
 })();
