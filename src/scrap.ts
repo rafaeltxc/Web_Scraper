5ef3afa9c24d99a3e104fa76a975b2cb871437a0
Rafael Costa <rafael.txct@gmail.com>
2023-03-17 21:20:57 -0300

    selenium

--- src/scrap.ts
+++ src/scrap.ts
@@ -1,6 +1,13 @@
 import { Builder, By, Key } from 'selenium-webdriver';
 
 require('chromedriver');
+interface objPattern {
+  imgSrc: string[] | null;
+  title: string | null;
+  src: string | null;
+  shop: string | null;
+  price: number | null;
+}
 
 (async function helloSelenium() {
   const driver = await new Builder().forBrowser('chrome').build();
@@ -10,4 +17,15 @@ require('chromedriver');
   const searchBar = await driver.findElement(By.className('gLFyf'));
   await searchBar.sendKeys('Bola de basquete');
   await searchBar.sendKeys(Key.ENTER);
+
+  const shopping = await driver.findElement(By.xpath('//*[contains(text(), "Shopping")]'));
+  await shopping.click();
+
+  const sponsoredElements = await driver.findElements(By.className('KZmu8e'));
+  const data: string[] = [];
+  for (let i = 0; i < sponsoredElements.length; i++) {
+    await sponsoredElements[i].click();
+  }
+
+  driver.close();
 })();
